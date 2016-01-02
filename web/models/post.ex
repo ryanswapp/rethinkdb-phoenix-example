e521373f02c44dfb1de687a763507e1f81cfe96d
Ryan Swapp <ryancswapp@gmail.com>
2016-01-02 16:29:08 -0700

    Added schema example

--- web/models/post.ex
+++ web/models/post.ex
@@ -4,11 +4,12 @@ defmodule RethinkExample.Post do
   schema "posts" do
     field :title, :string
     field :content, :string
+    field :user, :string
 
     timestamps
   end
 
-  @required_fields ~w(title content)
+  @required_fields ~w(title content user)
   @optional_fields ~w()
 
   @doc """
